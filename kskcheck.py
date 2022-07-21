d15322229a4bb10584ad577892fca1414d01c86e
Florin-MS <79944491+florinDNL@users.noreply.github.com>
2022-07-21 10:00:48 +0300

    Update kskcheck.py

--- kskcheck.py
+++ kskcheck.py
@@ -383,7 +383,7 @@ def allowed_file(filename):
 
 @app.route('/')
 def upload_form():
-    return render_template('upload.html')
+    return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def upload_file():
