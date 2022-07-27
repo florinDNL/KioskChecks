8b5759bbdff25761f958faa9fbf5f339f47fd822
florinDNL <dalianflorin@gmail.com>
2022-07-27 18:15:13 +0300

    -

--- kskcheck.py
+++ kskcheck.py
@@ -463,4 +463,4 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000,debug=True,threaded=True)
+    app.run(host='127.0.0.1',port=5000,debug=False,threaded=True)
