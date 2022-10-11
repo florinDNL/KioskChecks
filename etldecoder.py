51384793e1f0c69a2f7ab1dfb1721e59c6e43a98
florinDNL <dalianflorin@gmail.com>
2022-10-11 17:46:59 +0300

    -

--- etldecoder.py
+++ etldecoder.py
@@ -5,6 +5,11 @@ def decodeEtlTrace(folder, etl_trace):
     tracefmt = "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x64\\tracefmt.exe"    
     commandline = "{} {} -nosummary -o tmftrace.txt".format(tracefmt, os.path.join(folder, etl_trace))
     subprocess.run(commandline)
+    
+def parseLine(line):
+    separator1 = "::"
+    time = line.split(separator1, 1)
+    return time
 
 def parseTrace(folder, etl_trace):
     decodeEtlTrace(folder, etl_trace)
@@ -19,7 +24,8 @@ def parseTrace(folder, etl_trace):
             etl_report.append("Non-zero HRESULT events not found. Dumping all AssignedAccess events, error might not be among them.\n\n")
             for line in trace:
                 if "AssignedAccess" in line:
+                    line = parseLine(line)
                     etl_report.append(line)
-                    
+
     os.remove("tmftrace.txt")
     return etl_report
